c3c804453dad7b442e7533fc4298e3c39fe86bc4
TheGreatNobody <57835305+TheGreatNobody@users.noreply.github.com>
2024-08-08 00:56:06 +0800

    Initial

---
+++ src/main/java/tw/com/nobody/woodpecker/WoodpeckerApplication.java
@@ -0,0 +1,20 @@
+package tw.com.nobody.woodpecker;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@Log4j2
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class WoodpeckerApplication {
+
+	public static void main(String[] args) {
+		log.warn("Warning");
+		log.info("Info");
+		log.error("Error");
+		log.debug("Debug");
+		SpringApplication.run(WoodpeckerApplication.class, args);
+	}
+
+}
